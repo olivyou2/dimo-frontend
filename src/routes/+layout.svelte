ac5bc0c9b0520fc680c19082cc545af07598d5f4
박원호 <36305517+olivyou2@users.noreply.github.com>
2024-02-06 22:02:01 +0900

    refactor: header refactoring

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,5 @@
 <div id="body">
-    <div id="container">
-        <slot />
-    </div>
+    <slot />
 </div>
 
 <style>
@@ -11,18 +9,5 @@
             format("woff");
         font-weight: 400;
         font-style: normal;
-    }
-
-    #body {
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-    }
-
-    #container {
-        padding: 92px 40px 40px 40px;
-        padding-top: 92px;
-        max-width: 1224px;
-        width: 100%;
     }
 </style>
--- src/routes/components/Header.svelte
+++ src/routes/components/Header.svelte
@@ -1,118 +1,75 @@
 <script>
-    import Sort from "./Sort.svelte";
-
     /**
      * @description The categories of the header
      * @type {string[]}
      */
     export let categories;
+
+    let selectedCategory = categories[0];
+
+    /**
+     * @description The function that is called when a category is clicked
+     * @param {string} category The category that is clicked
+     */
+    function onClickCategory(category) {
+        selectedCategory = category;
+    }
 </script>
 
 <div id="container">
-    <div id="top">
-        <div id="category_container">
-            {#each categories as category, idx}
-                {#if idx == 0}
-                    <div class="category activated">{category}</div>
-                {:else}
-                    <div class="category">{category}</div>
-                {/if}
-            {/each}
-        </div>
-        <div id="logo">
-            <img src="/logo.svg" alt="" />
-        </div>
+    <img src="/logo.svg" alt="" />
+
+    <div id="categories">
+        {#each categories as category}
+            <button
+                on:click={() => onClickCategory(category)}
+                class="category {category === selectedCategory
+                    ? 'activated'
+                    : ''}">{category}</button
+            >
+        {/each}
     </div>
-    <div id="bottom">
-        <div id="search">
-            <div id="icon_wrapper">
-                <img src="/search.svg" alt="" />
-            </div>
-            <div id="input_wrapper">
-                <input id="input" type="text" placeholder="페이지 이름 검색" />
-            </div>
-        </div>
-        <Sort />
-    </div>
+
+    <img src="/icon.svg" alt="" />
 </div>
 
 <style lang="scss">
     #container {
-        widows: 100%;
+        width: calc(100vw - 85px);
+        height: 120px;
 
         display: flex;
-        flex-direction: column;
-        height: 150px;
+        flex-direction: row;
+
+        padding-left: 52px;
+        padding-right: 33px;
+
+        align-items: center;
         justify-content: space-between;
 
-        > #top {
+        background-color: white;
+        border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+
+        > #categories {
             display: flex;
-            justify-content: space-between;
-            > #category_container {
-                display: flex;
-                column-gap: 32px;
+            gap: 40px;
+            > .category {
+                color: rgba(0, 0, 0, 0.5);
+                font-family: Pretendard;
+                font-size: 24px;
+                font-style: normal;
+                font-weight: 500;
+                line-height: normal;
+                text-decoration: none;
 
-                > .category {
-                    color: rgba(0, 0, 0, 0.5);
-                    font-family: Pretendard;
-                    font-size: 24px;
-                    font-style: normal;
-                    font-weight: 400;
-                    line-height: normal;
-
-                    user-select: none;
-                    cursor: pointer;
-                }
-
-                > .category.activated {
-                    color: rgba(0, 0, 0, 0.8);
-                }
+                background: none;
+                border: none;
+                padding: 0px;
             }
 
-            > #logo {
-            }
-        }
-
-        > #bottom {
-            display: flex;
-            justify-content: space-between;
-
-            > #search {
-                display: flex;
-                column-gap: 20px;
-                flex: 1;
-
-                > #input_wrapper {
-                    flex: 1;
-                }
-
-                > #icon_wrapper,
-                > #input_wrapper {
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                }
-
-                #input {
-                    border: none;
-                    background: none;
-
-                    color: rgba(0, 0, 0, 0.7);
-                    font-family: Pretendard;
-                    font-size: 20px;
-                    font-style: normal;
-                    font-weight: 500;
-                    line-height: normal;
-                    flex: 1;
-                }
-
-                #input:focus {
-                    outline: none;
-                }
-
-                #input::placeholder {
-                    color: rgba(0, 0, 0, 0.3);
-                }
+            > .category.activated {
+                color: rgba(0, 0, 0, 0.8);
+                font-weight: 700;
             }
         }
     }
